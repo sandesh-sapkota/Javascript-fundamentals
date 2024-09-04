9582dea52b6734985b150064b736fc27e9a00299
sandesh-sapkota <sapkotasandeshlive12@gmail.com>
2024-09-04 22:15:48 +0545

    initial commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+ const accountId= 771707;
+ let accoundEmail="sandy@gmail.com";
+ 
+ var acccoundPassword="1234567"; //It is not recommended to use.
+
+//  accountId=101; not allowed to assign in constant
+
+
+ acccoundPassword="77777";
+ 
+  console.log(acccoundPassword);
+
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs, not browser so it results as undefined
+
+console.log(3 + 3);
+
+console.log("Sandy");
+
+
+let name="Sandy"
+let age=18
+let isLoggedIn=false
+
+let person="sandy" , carName="BMW" , price= 175;
+
+// number => 2^53
+// bigint => for very larger number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol=> unique
+ 
+//object
+
+console.log(typeof price); //number
+
+console.log(typeof "sandy"); //string
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
+
