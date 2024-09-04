781a34b0f77705750c5a00067a5d7b1211681a91
sandesh-sapkota <sapkotasandeshlive12@gmail.com>
2024-09-04 22:41:06 +0545

    initial commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score="33a"
+
+// console.log(typeof score);
+
+// let valueInNumber= Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33a" => NaN
+// true => 1; false => 0
+// null=> 0
+
+// let isLoggedIn=1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+//1=> true
+// 0=> false
+// "" => false
+// "sandy" => true
+
+
+let someNumber = 33
+
+let stringNumber= String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
